996c3f0f1f1da52d28e904db2935755166d7b515
Nikhil <nikhilkalakkara05@gmail.com>
2026-04-10 16:52:28 +0530

    updated code

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
+const local = 'http://localhost:5000'
+const production = 'https://backend-ecommerce-u26c.onrender.com'
+
+let api_url = ''
+let mode = 'pro'
+
+if(mode === 'pro')
+{
+    api_url = production
+}
+else{
+    api_url = local
+}
 
 const api = axios.create({
-  baseURL : 'http://localhost:5000/api'
+    baseURL : `${api_url}/api`,
+    withCredentials: true
 })
 
-export default api;
+export default api
